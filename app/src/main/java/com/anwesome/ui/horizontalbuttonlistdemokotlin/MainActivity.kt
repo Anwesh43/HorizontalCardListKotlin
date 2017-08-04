fd3760d010ccb3dbd62e9ad1c94e3a0a666758a8
Anwesh43 <anweshthecool0@gmail.com>
2017-08-05 04:42:25 +0530

    using HorizontalButtonListKotlin in activity

--- app/src/main/java/com/anwesome/ui/horizontalbuttonlistdemokotlin/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/horizontalbuttonlistdemokotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.anwesome.ui.horizontalbuttonlistdemokotlin
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesome.ui.kotlinhorizontalbuttonlist.HorizontalButtonList
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var bitmap = BitmapFactory.decodeResource(resources,R.drawable.mustafi)
+        HorizontalButtonList.create(this)
+        for(i in 1..10) {
+            HorizontalButtonList.addButton(bitmap,"Mustafi $i")
+        }
+        HorizontalButtonList.show(this)
     }
 }
